6102efda376b822fe54b0c993c054a0c2781eae5
Ramunas <rambek@gmail.com>
2021-02-12 11:06:22 +0200

    admin panel layout

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -8,14 +8,8 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div><!-- /.col -->
+            <h1 class="m-0">Pagrindinis</h1>
+          </div>
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
